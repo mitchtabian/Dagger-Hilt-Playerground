374f3f1b9a0b3125f6dd74a81541420b17438080
mitch <mitch@tabian.ca>
2020-06-30 14:29:57 -0700

    cleanup

--- app/src/main/java/com/codingwithmitch/daggerhiltplayground/business/data/cache/CacheDataSourceImpl.kt
+++ app/src/main/java/com/codingwithmitch/daggerhiltplayground/business/data/cache/CacheDataSourceImpl.kt
@@ -3,8 +3,6 @@ package com.codingwithmitch.daggerhiltplayground.business.data.cache
 import com.codingwithmitch.daggerhiltplayground.business.domain.models.Blog
 import com.codingwithmitch.daggerhiltplayground.framework.datasource.cache.database.BlogDao
 import com.codingwithmitch.daggerhiltplayground.framework.datasource.cache.mappers.CacheMapper
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.withContext
 
 class CacheDataSourceImpl
 constructor(
@@ -16,8 +14,8 @@ constructor(
         return blogDao.insert(cacheMapper.mapToEntity(blog))
     }
 
-    override suspend fun insertList(blogs: List<Blog>) = withContext(IO) {
-        for(blog in blogs){
+    override suspend fun insertList(blogs: List<Blog>){
+        for(blog in blogs) {
             blogDao.insert(cacheMapper.mapToEntity(blog))
         }
     }
