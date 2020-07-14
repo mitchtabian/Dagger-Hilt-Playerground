387bf613e7db976e473d644f6c51a6a343958816
mitch <mitch@tabian.ca>
2020-07-14 10:40:51 -0700

    Starting point

---
+++ app/src/main/java/com/codingwithmitch/daggerhiltplayground/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.codingwithmitch.daggerhiltplayground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "AppDebug"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
