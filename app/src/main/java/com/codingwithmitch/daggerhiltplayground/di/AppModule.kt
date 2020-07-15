1793c269071327311f9741e52f71e0be9e3feb24
mitch <mitch@tabian.ca>
2020-07-14 17:05:17 -0700

    How to: Fragment constructor injection with Hilt

---
+++ app/src/main/java/com/codingwithmitch/daggerhiltplayground/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.codingwithmitch.daggerhiltplayground.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideSomeString(): String{
+        return "It's some String!"
+    }
+}
---
+++ app/src/main/java/com/codingwithmitch/daggerhiltplayground/ui/MainFragment.kt
@@ -0,0 +1,68 @@
+package com.codingwithmitch.daggerhiltplayground.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.codingwithmitch.daggerhiltplayground.R
+import com.codingwithmitch.daggerhiltplayground.model.Blog
+import com.codingwithmitch.daggerhiltplayground.util.DataState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class MainFragment
+constructor(
+    private val someString: String
+): Fragment(R.layout.fragment_main) {
+
+    private val TAG: String = "AppDebug"
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeObservers()
+        viewModel.setStateEvent(MainStateEvent.GetBlogsEvent)
+
+        Log.d(TAG, "Hey look! ${someString}")
+    }
+
+    private fun subscribeObservers(){
+        viewModel.dataState.observe(viewLifecycleOwner, Observer { dataState ->
+            when(dataState){
+                is DataState.Success<List<Blog>> -> {
+                    displayProgressBar(false)
+                    appendBlogTitles(dataState.data)
+                }
+                is DataState.Error -> {
+                    displayProgressBar(false)
+                    displayError(dataState.exception.message)
+                }
+                is DataState.Loading -> {
+                    displayProgressBar(true)
+                }
+            }
+        })
+    }
+
+    private fun displayError(message: String?){
+        if(message != null) text.text = message else text.text = "Unknown error."
+    }
+
+    private fun appendBlogTitles(blogs: List<Blog>){
+        val sb = StringBuilder()
+        for(blog in blogs){
+            sb.append(blog.title + "\n")
+        }
+        text.text = sb.toString()
+    }
+
+    private fun displayProgressBar(isDisplayed: Boolean){
+        progress_bar.visibility = if(isDisplayed) View.VISIBLE else View.GONE
+    }
+}
